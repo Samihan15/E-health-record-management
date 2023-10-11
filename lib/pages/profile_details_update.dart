03b8dcc60ae94c65b85d9a61c59454ba5bb0fd70
Samihan <samihannandedkar15@gmail.com>
2023-10-12 01:16:32 +0530

    editing the personal data ui completed

---
+++ lib/pages/profile_details_update.dart
@@ -0,0 +1,132 @@
+import 'package:ehr_management/utils/constant.dart';
+import 'package:flutter/material.dart';
+
+class UpdateDetailsPage extends StatefulWidget {
+  const UpdateDetailsPage({super.key});
+
+  @override
+  State<UpdateDetailsPage> createState() => _UpdateDetailsPageState();
+}
+
+class _UpdateDetailsPageState extends State<UpdateDetailsPage> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _ageController = TextEditingController();
+  TextEditingController _bloodController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _nameController.dispose();
+    _ageController.dispose();
+    _bloodController.dispose();
+    _addressController.dispose();
+    _phoneController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'U P D A T E   I N F O R M A T I O N',
+          style: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                hintText: 'update your name',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: black, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _ageController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Age',
+                hintText: 'update your Age',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: black, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _bloodController,
+              decoration: InputDecoration(
+                labelText: 'Blood Group',
+                hintText: 'update your blood group',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: black, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _addressController,
+              decoration: InputDecoration(
+                labelText: 'Address',
+                hintText: 'update your address',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: black, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            TextField(
+              controller: _phoneController,
+              decoration: InputDecoration(
+                labelText: 'Phone Number',
+                hintText: 'update your phone number',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: black, width: 2),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // update information logic
+                Navigator.of(context).pop();
+              },
+              style: ButtonStyle(elevation: MaterialStateProperty.all(2)),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 60.0, vertical: 15),
+                child: Text(
+                  'Update',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
